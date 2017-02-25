001237e3089e89f25326459d7b8c7de3d5769ca2
WilliamDiouf <williamdiouf@yahoo.fr>
2017-02-25 09:55:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$.ajax({
-		url : "https://blockchain.info/fr/api/blockchain_api,
+		url : "https://blockchain.info/fr/api/blockchain_api",
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
